ba535b3042a3cc2946b9a7b720e5378652507a56
Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-06-07 12:45:05 +0300

    Switches

--- src/by/epamtc/zhurava_skarabahataya/task_02/Main.java
+++ src/by/epamtc/zhurava_skarabahataya/task_02/Main.java
@@ -3,7 +3,50 @@ package by.epamtc.zhurava_skarabahataya.task_02;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("njfnej");
+		int dayNumber;
+		dayNumber = 43;
+
+		determineDate(dayNumber);
+	}
+
+	private static void determineDate(int dayNumber) {
+		int monthNumber;
+		int daysInCurrentMonth;
+
+		monthNumber = 1;
+		daysInCurrentMonth = getDaysInMonth(monthNumber);
+
+		while (dayNumber > daysInCurrentMonth) {
+			dayNumber -= daysInCurrentMonth;
+			monthNumber++;
+			daysInCurrentMonth = getDaysInMonth(monthNumber);
+
+		}
+		
+		System.out.println(dayNumber + "." + monthNumber);
+	}
+
+	private static int getDaysInMonth(int month) {
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			return 31;
+		case 2:
+			return 28;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			return 30;
+		default:
+			return -1;
+		}
+
 	}
 
 }
--- src/by/epamtc/zhurava_skarabahataya/task_06/Main.java
+++ src/by/epamtc/zhurava_skarabahataya/task_06/Main.java
@@ -1,7 +1,7 @@
 /*
- * Сумма первых n членов арифметической прогрессии вычисляется по формуле Sn = ( a1 + an )* n / 2. 
- * Даны a1 и d (шаг арифметической прогрессии). 
- * Найдите n, при котором значение Sn выходит за диапазон типов int и long (экспериментальным путем).
+ * пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ n пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ Sn = ( a1 + an )* n / 2. 
+ * пїЅпїЅпїЅпїЅ a1 пїЅ d (пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ). 
+ * пїЅпїЅпїЅпїЅпїЅпїЅпїЅ n, пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ Sn пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ int пїЅ long (пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ).
 */
 
 package by.epamtc.zhurava_skarabahataya.task_06;
@@ -9,8 +9,8 @@ package by.epamtc.zhurava_skarabahataya.task_06;
 public class Main {
 
 	public static void main(String[] args) {
-		double a1;
-		double d;
+		long a1;
+		long d;
 
 		a1 = 1000;
 		d = 340;
@@ -18,7 +18,7 @@ public class Main {
 		findN(a1, d);
 	}
 
-	private static void findN(double a1, double d) {
+	private static void findN(long a1, long d) {
 		boolean isInIntRange;
 		boolean isInLongRange;
 		long n;
@@ -35,12 +35,12 @@ public class Main {
 			sn = (a1 + an) * n / 2;
 
 			if (isInIntRange && (sn > Integer.MAX_VALUE || sn < Integer.MIN_VALUE)) {
-				System.out.printf("Sn выходит за пределы int при n=%d%n", n);
+				System.out.printf("Sn пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ int пїЅпїЅпїЅ n=%d%n", n);
 				isInIntRange = false;
 			}
 			if (sn > Long.MAX_VALUE || sn < Long.MIN_VALUE) {
 				isInLongRange = false;
-				System.out.printf("Sn выходит за пределы long при n=%d%n", n);
+				System.out.printf("Sn пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ long пїЅпїЅпїЅ n=%d%n", n);
 			}
 
 			n++;
